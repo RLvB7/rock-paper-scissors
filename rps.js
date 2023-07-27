fdc314044f973ed7db17cd1c6d5f641a4b7dbc96
RLvB <138893006+RLvB7@users.noreply.github.com>
2023-07-27 18:07:11 +0100

    Add core game mechanic

--- rps.js
+++ rps.js
@@ -2,9 +2,21 @@ function getComputerChoice() {
 	return Math.floor(Math.random() * 3);
 }
 
+function play(playerChoice, computerChoice) {
+	if (playerChoice === computerChoice) {
+		return `Draw! ${choices[playerChoice]} ties with ${choices[computerChoice]}!`;
+	} else if (playerChoice === computerChoice - 1 || playerChoice === computerChoice + 2) {
+		return `You lose! ${choices[computerChoice]} beats ${choices[playerChoice]}!`;
+	} else {
+		return `You win! ${choices[playerChoice]} beats ${choices[computerChoice]}!`;
+	}
+}
+
 const choices = ['Rock', 'Paper', 'Scissors'];
 
+let playerChoice = 0;
 let computerChoice = getComputerChoice();
-let computerChoiceText = choices[computerChoice];
 
-console.log(computerChoiceText);
+let outcome = play(playerChoice, computerChoice);
+
+console.log(outcome);
